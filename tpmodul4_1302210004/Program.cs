73d57b7484ac0ac913339664cd5cd06c1c239aae
Jeanrikha <jeanrikha@student.telkomuniversity.ac.id>
2023-03-12 22:10:38 +0700

    [200~“menambahkan implementasi dengan state-based

--- tpmodul4_1302210004/Program.cs
+++ tpmodul4_1302210004/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using static KodePos;
 
 class KodePos
 {
     private Dictionary<string, string> kodepos = new Dictionary<string, string>()
     {
         { "Batununggal", "40266" },
-        { "A. Kujangsari", "40287" },
+        { "Kujangsari", "40287" },
         { "Mengger", "40267" },
         { "Wates", "40256" },
         { "Cijaura", "40287" },
@@ -29,13 +30,54 @@ class KodePos
             return "Kode pos tidak ditemukan.";
         }
     }
+
+    public class DoorMachine
+    {
+        private State state;
+
+        public DoorMachine()
+        {
+            state = State.Locked;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void Lock()
+        {
+            if (state == State.Unlocked)
+            {
+                state = State.Locked;
+                Console.WriteLine("Pintu terkunci");
+            }
+        }
+
+        public void Unlock()
+        {
+            if (state == State.Locked)
+            {
+                state = State.Unlocked;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+        }
+
+        private enum State
+        {
+            Locked,
+            Unlocked
+        }
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("================TABLE-DRIVEN================\t");
         KodePos kodepos = new KodePos();
-        Console.WriteLine(kodepos.getKodePos("Batununggal")); // output: 40266
+        Console.WriteLine("Kode Pos Batununggal adalah: \n" + kodepos.getKodePos("Batununggal") + "\n"); // output: Kode Pos Batununggal adalah: 40266
+        Console.WriteLine("==========STATE-BASED CONSTRUCTION==========");
+        DoorMachine pintu = new DoorMachine();
+        pintu.Lock();
+        pintu.Unlock();
+        Console.ReadLine();
     }
 }
