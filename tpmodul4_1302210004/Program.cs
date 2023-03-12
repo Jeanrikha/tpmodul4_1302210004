903065b2be1034f3ff0cccd24523d9bfa6c9c611
Jeanrikha <jeanrikha@student.telkomuniversity.ac.id>
2023-03-12 21:04:21 +0700

    inisialisasi projek

---
+++ tpmodul4_1302210004/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private Dictionary<string, string> kodepos = new Dictionary<string, string>()
+    {
+        { "Batununggal", "40266" },
+        { "A. Kujangsari", "40287" },
+        { "Mengger", "40267" },
+        { "Wates", "40256" },
+        { "Cijaura", "40287" },
+        { "Jatisari", "40286" },
+        { "Margasari", "40286" },
+        { "Sekejati", "40286" },
+        { "Kebonwaru", "40272" },
+        { "Maleer", "40274" },
+        { "Samoja", "40273" },
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        if (kodepos.ContainsKey(kelurahan))
+        {
+            return kodepos[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan.";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodepos = new KodePos();
+        Console.WriteLine(kodepos.getKodePos("Batununggal")); // output: 40266
+    }
+}
